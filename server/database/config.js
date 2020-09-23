c81c374c0e40900ab2af58772f66daefedbbfcab
samridh <samridhharsh@gmail.com>
2020-09-23 15:06:55 +0530

    deployment server prepared

---
+++ server/database/config.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const url = "mongodb+srv://sam:sam@cluster0.qcgnm.mongodb.net/couponcodevalidator?retryWrites=true&w=majority"
+
+const open = (uri = url) => {
+
+    return new Promise((resolve, reject) => {
+        mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true  }, err => {
+            if (err) return reject(err)
+            console.log('connected to db!')
+            resolve()
+        })
+    })
+}
+
+
+const close = () => mongoose.disconnect()
+
+module.exports = { open, close }
